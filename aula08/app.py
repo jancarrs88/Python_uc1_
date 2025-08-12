46402336744a6141d48628b9900f8c3a2d14f8cf
jancarrs88 <jancarrs88@gmail.com>
2025-08-12 20:34:03 -0300

    Aula 08 - Desafio 08 12\08\2025

---
+++ aula08/app.py
@@ -0,0 +1,25 @@
+# Desafio 08
+# Atividade 01
+# Defina a função saudação sem parâmetros.
+# Dentro da função, use o print para exibir a mensagem.
+# Chamar a função para ver o resultado
+
+"""
+Função Saudação
+    - Imprime uma mensagem de boas vindas
+"""
+
+def saudacao():
+   print(f"\n\n\tOlá, seja bem vindo ao curso de Python!\n\n")
+
+
+def soma(a,b):
+  return a+b
+resultado = soma(5, 7)
+#print(f"A soma é: {resultado}")
+
+  
+
+##
+# Corpo do Programa
+#saudacao()
