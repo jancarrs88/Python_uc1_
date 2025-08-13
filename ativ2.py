e77236d70f84bfe5b00fd9a7ca0560f288ba0ad1
jancarrs88 <jancarrs88@gmail.com>
2025-08-12 21:49:25 -0300

    Update ativ2.py

--- ativ2.py
+++ ativ2.py
@@ -1,11 +1,12 @@
+""""
 def saudacao():
    print("\n\n\tOlá, mundo!\n")
 
 
 def soma(a,b):
   return a+b
-#resultado = soma(5, 7)
-#print(f"A soma é: {resultado}")
+resultado = soma(5, 7)
+print(f"A soma é: {resultado}")
 
 
 
@@ -18,5 +19,20 @@ def somar_numeros_v3():
    soma = num1 + num2
    print(f"A soma é: {soma}")
 
+"""
+
+def soma(a, b):
+   return a + b
+
+def subtracao(a, b):
+   return a - b
+
+def main():
+   resultado_soma = soma(5, 5)
+   resultado_subtracao = subtracao(25, 5)
+   print(f"Soma: >>{resultado_soma}<<")
+   print(f"Subtração: >>{resultado_subtracao}<<")
+
+
 if __name__ == "__main__":
-   somar_numeros_v3()
+   main()
